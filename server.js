1d3ac26522a05a8a103350dd60b73ed5ec309f97
amitbappa <amitbappa@gmail.com>
2020-08-24 11:33:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const io = require('socket.io')(server)
 const YT_API_KEY='AIzaSyCG25IwSEZcJuF5Te7kko9XawkHaEJ48Ws'
 var YT_VIDEO_ID=''
 
-
-app.use('/', express.static('public'))
-
 io.on('connection', (socket) => {
   socket.on('join', (roomId) => {
     const roomClients = io.sockets.adapter.rooms[roomId] || { length: 0 }
@@ -18,11 +15,11 @@ io.on('connection', (socket) => {
       YT_VIDEO_ID='F5UPc8dya-M'
       console.log(`Creating room ${roomId} and emitting room_created socket event`)
       socket.join(roomId)
-      socket.emit('room_created', {roomId,YT_VIDEO_ID,YT_API_KEY})
+      socket.emit('room_created', {roomId,YT_API_KEY})
     } else if (numberOfClients == 1) {
       console.log(`Joining room ${roomId} and emitting room_joined socket event`)
       socket.join(roomId)
-      socket.emit('room_joined', {roomId,YT_VIDEO_ID,YT_API_KEY})
+      socket.emit('room_joined', {roomId,YT_API_KEY})
     } else {
       console.log(`Can't join room ${roomId}, emitting full_room socket event`)
       socket.emit('full_room', roomId)
@@ -33,13 +30,13 @@ io.on('connection', (socket) => {
  
   socket.on('set_current', function(data) {
             console.log(`Broadcasting set_current event to peers in room ${data.roomId}`)
-            YT_VIDEO_ID= data.YT_VIDEO_ID   
-        io.sockets.in(data.roomId).emit('play_video',{roomId:data.roomId,YT_VIDEO_ID:data.YT_VIDEO_ID,isRoomCreator:true})
+
+        io.sockets.in(data.roomId).emit('play_video',{roomId:data.roomId,yt_video_id:YT_VIDEO_ID,isRoomCreator:true})
     })
 
     socket.on('get_current', (roomId)=> {
             console.log(`Broadcasting get_current event to peers in room ${roomId}`)
-          io.sockets.in(roomId).emit('play_video',{roomId:roomId,YT_VIDEO_ID:YT_VIDEO_ID,isRoomCreator:false})
+          io.sockets.in(roomId).emit('play_video',{roomId:roomId,yt_video_id:YT_VIDEO_ID,isRoomCreator:false})
     })
 })
 
@@ -48,4 +45,3 @@ const port = process.env.PORT || 5000
 server.listen(port, () => {
   console.log(`Express server listening on port ${port}`)
 })
-
